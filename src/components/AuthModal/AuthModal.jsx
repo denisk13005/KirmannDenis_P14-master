9e28572c1cd39bfacfe972eefe89d9e758397208
kirmann <k.dkle@hotmail.fr>
2022-05-08 11:48:47 +0200

    auth ok

--- src/components/AuthModal/AuthModal.jsx
+++ src/components/AuthModal/AuthModal.jsx
@@ -1,34 +1,63 @@
-import React, { useState } from "react"
-import { useNavigate } from "react-router-dom"
-// import { useDispatch } from "react-redux"
+import React, { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import "./authModal.scss"
 import { signInAuth } from "../../utils/apiDbFiresbase"
-// import { log } from "../../features/employee"
+import { log } from "../../features/employee"
 
 const AuthModal = () => {
+  const adminLoggedIn = useSelector((state) => state.employees.adminLoggedIn)
+  const [userName, setUserName] = useState(" ")
+  const [password, setPassword] = useState("")
   const [errorMessage, setErrorMessage] = useState("")
-  const navigate = useNavigate()
-  // const dispatch = useDispatch()
+  const dispatch = useDispatch()
   const logged = async (e) => {
+    console.log(userName, password)
     e.preventDefault()
-    const auth = await signInAuth("test@test.com", "password")
+    getAuth(userName, password)
+  }
+
+  const getAuth = async (userName, password) => {
+    const auth = await signInAuth(userName, password)
     if (auth.accessToken) {
-      navigate("/CreateEmployee")
+      dispatch(log())
       setErrorMessage("")
     } else {
-      setErrorMessage(auth)
+      setErrorMessage(auth.split("/")[1])
     }
   }
 
+  //unmount when loggedIn
+  useEffect(() => {
+    return () => null
+  }, [adminLoggedIn])
+
   return (
     <div className="authModal">
-      <form onSubmit={logged}>
-        <label htmlFor="userName">Username</label>
-        <input type="text" name="userName" id="userName" />
-        <label htmlFor="userName">Password</label>
-        <input type="password" name="userName" id="userName" />
-        <span className="error">{errorMessage}</span>
-        <input type="submit" />
+      <form className="authForm" onSubmit={logged}>
+        <h2 id="loginTitle">LogIn</h2>
+        <label className="authLabel" htmlFor="userName">
+          Username
+        </label>
+        <input
+          type="text"
+          name="userName"
+          id="userName"
+          value={userName}
+          placeholder="test"
+          onChange={(e) => setUserName(e.target.value.trim())}
+        />
+        <label className="authLabel" htmlFor="password">
+          Password
+        </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value.trim())}
+        />
+        <span className="errorMessage">{errorMessage}</span>
+        <input className="submitBtn" type="submit" value="Se Connecter" />
       </form>
     </div>
   )
