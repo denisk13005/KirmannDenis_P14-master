67bc91d7155d724a94ea3b2c4500ab5e4f368b2a
kirmann <k.dkle@hotmail.fr>
2022-05-14 16:34:34 +0200

    ajout config eslint

--- src/utils/firebase.config.js
+++ src/utils/firebase.config.js
@@ -2,28 +2,20 @@ import { initializeApp } from "firebase/app"
 import { getFirestore, collection } from "@firebase/firestore"
 import { getAuth } from "firebase/auth"
 const firebaseConfig = {
-  // eslint-disable-next-line no-undef
   apiKey: process.env.REACT_APP_API_KEY,
 
-  // eslint-disable-next-line no-undef
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
 
-  // eslint-disable-next-line no-undef
   databaseURL: process.env.REACT_APP_DATABASE_URL,
 
-  // eslint-disable-next-line no-undef
   projectId: process.env.REACT_APP_PROJECT_ID,
 
-  // eslint-disable-next-line no-undef
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
 
-  // eslint-disable-next-line no-undef
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 
-  // eslint-disable-next-line no-undef
   appId: process.env.REACT_APP_APP_ID,
 
-  // eslint-disable-next-line no-undef
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 }
 console.log(firebaseConfig.apiKey, firebaseConfig.projectId)
